db1bcf81643c2ebb2122a5005c1dabeac233d617
veinhorn <b.korogvich@gmail.com>
2017-01-16 21:24:34 +0300

    improved nav drawer

--- app/src/main/java/com/veinhorn/tikiticket/android/NavigationDrawer.java
+++ app/src/main/java/com/veinhorn/tikiticket/android/NavigationDrawer.java
@@ -3,28 +3,48 @@ package com.veinhorn.tikiticket.android;
 import android.app.Activity;
 import android.support.v7.widget.Toolbar;
 
+import com.mikepenz.fontawesome_typeface_library.FontAwesome;
+import com.mikepenz.materialdrawer.AccountHeader;
+import com.mikepenz.materialdrawer.AccountHeaderBuilder;
 import com.mikepenz.materialdrawer.Drawer;
 import com.mikepenz.materialdrawer.DrawerBuilder;
+import com.mikepenz.materialdrawer.model.ProfileDrawerItem;
 
 /**
  * Created by veinhorn on 12.1.17.
- * Holds all nav drawer building logic
+ * Contains all navigation drawer building logic
  */
 
-public class NavigationDrawer {
+class NavigationDrawer {
     private Activity activity;
     private Toolbar toolbar;
 
-    public NavigationDrawer(Activity activity,
+    NavigationDrawer(Activity activity,
                             Toolbar toolbar) {
         this.activity = activity;
         this.toolbar = toolbar;
     }
 
-    public Drawer build() {
+    Drawer build() {
         return new DrawerBuilder()
                 .withActivity(activity)
                 .withToolbar(toolbar)
+                .withAccountHeader(buildAccountHeader())
                 .build();
+    }
+
+    private AccountHeader buildAccountHeader() {
+        return new AccountHeaderBuilder()
+                .withActivity(activity)
+                .withHeaderBackground(R.drawable.header_background)
+                .addProfiles(buildProfileItem())
+                .withSelectionListEnabled(false)
+                .build();
+    }
+
+    private ProfileDrawerItem buildProfileItem() {
+        return new ProfileDrawerItem()
+                .withName("MCKLAS")
+                .withIcon(FontAwesome.Icon.faw_user);
     }
 }
