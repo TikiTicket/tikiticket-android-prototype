33f0f2a6166c9b2849438b4200d1eeb70754e704
veinhorn <b.korogvich@gmail.com>
2017-01-17 21:59:01 +0300

    added click listener for login button

--- app/src/main/java/com/veinhorn/tikiticket/android/LoginActivity.java
+++ app/src/main/java/com/veinhorn/tikiticket/android/LoginActivity.java
@@ -2,6 +2,12 @@ package com.veinhorn.tikiticket.android;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * Created by veinhorn on 16.1.17.
@@ -9,10 +15,25 @@ import android.os.Bundle;
  */
 
 public class LoginActivity extends Activity {
+    @BindView(R.id.loginEditText)
+    protected EditText loginEditText;
+
+    @BindView(R.id.passwordEditText)
+    protected EditText passwordEditText;
+
+    @BindView(R.id.signInButton)
+    protected Button signInButton;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+
+    }
+
+    @OnClick(R.id.signInButton)
+    protected void signIn() {
         new Authenticator(this).execute();
     }
 
